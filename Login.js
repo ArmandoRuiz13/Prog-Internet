d3263248cb2bf90365f60d87dd9722ff932562c7
Armando Ruiz Anaya <99087774+ArmandoRuiz13@users.noreply.github.com>
2022-08-23 12:49:16 -0500

    Actualizacion login

--- Login.js
+++ Login.js
@@ -1,6 +1,6 @@
 //importacion de los objetos
 import React, { Component } from 'react';
-import { View, Text ,StyleSheet,Image,TextInput} from 'react-native';
+import { View, Text ,StyleSheet,Image,TextInput, Button} from 'react-native';
 
 export default class Login extends Component {
   constructor(props) {
@@ -12,17 +12,26 @@ export default class Login extends Component {
 
   render() {
       //programacion en JS de los objetos
+      const btnClick = () => {
+        console.log("Dijiste click");
+      }
     return (
       <View>
         <Text style={styles.textoudg}> UDG </Text>
         <Image style={styles.logoudg} source={require("./Imagenes/logoudg.png")}/>
         <TextInput style={styles.input} placeholder="codigo" />
         <TextInput style={styles.input} placeholder="nip" secureTextEntry={true}/>
+        <View style={styles.boton}>
+          <Button title="Ingresar" onPress={btnClick}></Button>
+        </View>
       </View>
     );
   }
 }//declaracion de variables
 const styles = StyleSheet.create({
+screen:{
+  backgroundColor: "blue",
+},
 textoudg:{
 fontSize:40,
 color:"orange",
@@ -38,5 +47,11 @@ input:{
     borderWidth: 2,
     fontSize:25,
     marginTop:10
+},
+boton:{
+  width:100,
+  height:80,
+  marginLeft:150,
+  marginTop:30,
 },
 })
